98a8ddbc374a9f94e3665fdcd93704ff5a8866ec
surajchoudh <surajchoudhary604@gmail.com>
2024-01-27 23:25:17 +0530

    update

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const connection = await mongoose.connect('mongodb://127.0.0.1:27017/InventoryBilling', {
+        const connection = await mongoose.connect(process.env.MONGODB_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
